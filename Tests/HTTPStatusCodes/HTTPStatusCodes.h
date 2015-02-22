0f48fac9f7ed95aa314e3a937ab83ab3c530f1f3
Rich Hodgkins <rhodgkins@gmail.com>
2015-02-22 10:05:17 +0000

    Setup for Travis CI

--- Tests/HTTPStatusCodes/HTTPStatusCodes.h
+++ Tests/HTTPStatusCodes/HTTPStatusCodes.h
@@ -6,14 +6,11 @@
 //  Copyright (c) 2015 Rich H. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for HTTPStatusCodes.
 FOUNDATION_EXPORT double HTTPStatusCodesVersionNumber;
 
 //! Project version string for HTTPStatusCodes.
 FOUNDATION_EXPORT const unsigned char HTTPStatusCodesVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <HTTPStatusCodes/PublicHeader.h>
-
 

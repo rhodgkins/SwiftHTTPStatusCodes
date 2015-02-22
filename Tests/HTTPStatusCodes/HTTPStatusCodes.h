0fa9efe8f33a3c186e83f342fd6c7d5bcdb61ccc
Rich Hodgkins <rhodgkins@gmail.com>
2015-02-22 09:53:19 +0000

    Added tests project

---
+++ Tests/HTTPStatusCodes/HTTPStatusCodes.h
@@ -0,0 +1,19 @@
+//
+//  HTTPStatusCodes.h
+//  HTTPStatusCodes
+//
+//  Created by Richard Hodgkins on 22/02/2015.
+//  Copyright (c) 2015 Rich H. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HTTPStatusCodes.
+FOUNDATION_EXPORT double HTTPStatusCodesVersionNumber;
+
+//! Project version string for HTTPStatusCodes.
+FOUNDATION_EXPORT const unsigned char HTTPStatusCodesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPStatusCodes/PublicHeader.h>
+
+
---
+++ Tests/HTTPStatusCodesTests/HTTPStatusCodesTests.swift
@@ -0,0 +1,28 @@
+//
+//  HTTPStatusCodesTests.swift
+//  HTTPStatusCodesTests
+//
+//  Created by Richard Hodgkins on 22/02/2015.
+//  Copyright (c) 2015 Rich H. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class HTTPStatusCodesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+     
+    }
+}

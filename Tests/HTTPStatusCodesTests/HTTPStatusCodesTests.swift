452747fb162dc280b7b84fe9263bd55b0524200d
Rich Hodgkins <rhodgkins@gmail.com>
2016-10-12 20:17:42 +0100

    Added test case to improve test coverage

--- Tests/HTTPStatusCodesTests/HTTPStatusCodesTests.swift
+++ Tests/HTTPStatusCodesTests/HTTPStatusCodesTests.swift
@@ -21,47 +21,58 @@ import XCTest
 import HTTPStatusCodes
 #endif
 
-private func response(_ statusCode: Int) -> HTTPURLResponse {
+private func responseUsingInt(_ statusCode: Int) -> HTTPURLResponse {
     return HTTPURLResponse(url: URL(string: "http://www.google.com")!, statusCode: statusCode, httpVersion: nil, headerFields: nil)!
 }
-private func response(_ statusCode: HTTPStatusCode) -> HTTPURLResponse {
-    return HTTPURLResponse(url: NSURL(string: "http://www.google.com")! as URL, statusCode: statusCode.rawValue, httpVersion: nil, headerFields: nil)!
+private func responseUsingRawValue(_ statusCode: HTTPStatusCode) -> HTTPURLResponse {
+    return HTTPURLResponse(url: URL(string: "http://www.google.com")!, statusCode: statusCode.rawValue, httpVersion: nil, headerFields: nil)!
+}
+private func responseUsingEnum(_ statusCode: HTTPStatusCode) -> HTTPURLResponse {
+    return HTTPURLResponse(url: URL(string: "http://www.google.com")!, statusCode: statusCode, HTTPVersion: nil, headerFields: nil)!
 }
 
 final class HTTPStatusCodesTests: XCTestCase {
     
-    func testNSHTTPURLResponseInit() {
-        XCTAssertEqual(response(.continue).statusCode, 100, "Incorrect status code")
-        XCTAssertEqual(response(.ok).statusCode, 200, "Incorrect status code")
-        XCTAssertEqual(response(.multipleChoices).statusCode, 300, "Incorrect status code")
-        XCTAssertEqual(response(.badRequest).statusCode, 400, "Incorrect status code")
-        XCTAssertEqual(response(.internalServerError).statusCode, 500, "Incorrect status code")
+    func testNSHTTPURLResponseInitUsingInt() {
+        XCTAssertEqual(responseUsingRawValue(.continue).statusCode, 100, "Incorrect status code")
+        XCTAssertEqual(responseUsingRawValue(.ok).statusCode, 200, "Incorrect status code")
+        XCTAssertEqual(responseUsingRawValue(.multipleChoices).statusCode, 300, "Incorrect status code")
+        XCTAssertEqual(responseUsingRawValue(.badRequest).statusCode, 400, "Incorrect status code")
+        XCTAssertEqual(responseUsingRawValue(.internalServerError).statusCode, 500, "Incorrect status code")
+    }
+    
+    func testNSHTTPURLResponseInitUsingEnum() {
+        XCTAssertEqual(responseUsingEnum(.continue).statusCode, 100, "Incorrect status code")
+        XCTAssertEqual(responseUsingEnum(.ok).statusCode, 200, "Incorrect status code")
+        XCTAssertEqual(responseUsingEnum(.multipleChoices).statusCode, 300, "Incorrect status code")
+        XCTAssertEqual(responseUsingEnum(.badRequest).statusCode, 400, "Incorrect status code")
+        XCTAssertEqual(responseUsingEnum(.internalServerError).statusCode, 500, "Incorrect status code")
     }
     
     func testComputedStatusCodeValueProperty() {
-        XCTAssertEqual(response(100).statusCodeValue!, HTTPStatusCode.continue, "Incorrect status code")
-        XCTAssertEqual(response(200).statusCodeValue!, HTTPStatusCode.ok, "Incorrect status code")
-        XCTAssertEqual(response(300).statusCodeValue!, HTTPStatusCode.multipleChoices, "Incorrect status code")
-        XCTAssertEqual(response(400).statusCodeValue!, HTTPStatusCode.badRequest, "Incorrect status code")
-        XCTAssertEqual(response(500).statusCodeValue!, HTTPStatusCode.internalServerError, "Incorrect status code")
+        XCTAssertEqual(responseUsingInt(100).statusCodeValue!, HTTPStatusCode.continue, "Incorrect status code")
+        XCTAssertEqual(responseUsingInt(200).statusCodeValue!, HTTPStatusCode.ok, "Incorrect status code")
+        XCTAssertEqual(responseUsingInt(300).statusCodeValue!, HTTPStatusCode.multipleChoices, "Incorrect status code")
+        XCTAssertEqual(responseUsingInt(400).statusCodeValue!, HTTPStatusCode.badRequest, "Incorrect status code")
+        XCTAssertEqual(responseUsingInt(500).statusCodeValue!, HTTPStatusCode.internalServerError, "Incorrect status code")
     }
     
 #if DEBUG
     func testComputedStatusCodeEnumProperty() {
-        XCTAssertEqual(response(100).statusCodeEnum, HTTPStatusCode.continue, "Incorrect status code")
-        XCTAssertEqual(response(200).statusCodeEnum, HTTPStatusCode.ok, "Incorrect status code")
-        XCTAssertEqual(response(300).statusCodeEnum, HTTPStatusCode.multipleChoices, "Incorrect status code")
-        XCTAssertEqual(response(400).statusCodeEnum, HTTPStatusCode.badRequest, "Incorrect status code")
-        XCTAssertEqual(response(500).statusCodeEnum, HTTPStatusCode.internalServerError, "Incorrect status code")
+        XCTAssertEqual(responseUsingInt(100).statusCodeEnum, HTTPStatusCode.continue, "Incorrect status code")
+        XCTAssertEqual(responseUsingInt(200).statusCodeEnum, HTTPStatusCode.ok, "Incorrect status code")
+        XCTAssertEqual(responseUsingInt(300).statusCodeEnum, HTTPStatusCode.multipleChoices, "Incorrect status code")
+        XCTAssertEqual(responseUsingInt(400).statusCodeEnum, HTTPStatusCode.badRequest, "Incorrect status code")
+        XCTAssertEqual(responseUsingInt(500).statusCodeEnum, HTTPStatusCode.internalServerError, "Incorrect status code")
     }
 #endif
 
     func testHTTPStatusCodeInit() {
-        XCTAssertEqual(HTTPStatusCode(HTTPResponse: response(100))!, HTTPStatusCode.continue, "Incorrect status code")
-        XCTAssertEqual(HTTPStatusCode(HTTPResponse: response(200))!, HTTPStatusCode.ok, "Incorrect status code")
-        XCTAssertEqual(HTTPStatusCode(HTTPResponse: response(300))!, HTTPStatusCode.multipleChoices, "Incorrect status code")
-        XCTAssertEqual(HTTPStatusCode(HTTPResponse: response(400))!, HTTPStatusCode.badRequest, "Incorrect status code")
-        XCTAssertEqual(HTTPStatusCode(HTTPResponse: response(500))!, HTTPStatusCode.internalServerError, "Incorrect status code")
+        XCTAssertEqual(HTTPStatusCode(HTTPResponse: responseUsingInt(100))!, HTTPStatusCode.continue, "Incorrect status code")
+        XCTAssertEqual(HTTPStatusCode(HTTPResponse: responseUsingInt(200))!, HTTPStatusCode.ok, "Incorrect status code")
+        XCTAssertEqual(HTTPStatusCode(HTTPResponse: responseUsingInt(300))!, HTTPStatusCode.multipleChoices, "Incorrect status code")
+        XCTAssertEqual(HTTPStatusCode(HTTPResponse: responseUsingInt(400))!, HTTPStatusCode.badRequest, "Incorrect status code")
+        XCTAssertEqual(HTTPStatusCode(HTTPResponse: responseUsingInt(500))!, HTTPStatusCode.internalServerError, "Incorrect status code")
     }
     
     func testValidBooleanRangeChecks() {
@@ -165,7 +176,7 @@ final class HTTPStatusCodesTests: XCTestCase {
     func testBadStatusCodes() {
         XCTAssertTrue(HTTPStatusCode(rawValue: 1000) == nil)
         XCTAssertTrue(HTTPStatusCode(HTTPResponse: nil) == nil)
-        XCTAssertTrue(HTTPStatusCode(HTTPResponse: response(1000)) == nil)
+        XCTAssertTrue(HTTPStatusCode(HTTPResponse: responseUsingInt(1000)) == nil)
     }
 }
 

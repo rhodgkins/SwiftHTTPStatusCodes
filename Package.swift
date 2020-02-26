19bb8be562c88274c4ea16d26ad1f34144b8a0a1
Peter Thomas Horn <peter.horn@open-xchange.com>
2020-02-26 10:33:19 +0100

    Fixes #15: Update for Swift 5.1 toolchain.
    - Added build targets
    - Added installation instructions to README
    - Added library product to package description

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:4.0
 //
 //  Created by Richard Hodgkins on 07/06/2016.
 //  Copyright (c) 2016 Richard Hodgkins. All rights reserved.
@@ -6,5 +7,23 @@
 import PackageDescription
 
 let package = Package(
-  name: "HTTPStatusCodes"
+    name: "HTTPStatusCodes",
+    products: [
+        .library(name: "HTTPStatusCodes", targets: ["HTTPStatusCodes"])
+    ],
+    targets: [
+        .target(
+            name: "HTTPStatusCodes",
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "HTTPStatusCodesTests",
+            dependencies: ["HTTPStatusCodes"],
+            path: "Tests/HTTPStatusCodesTests",
+            sources: [
+                "HTTPStatusCodesTests.swift",
+                "RegressionTests.swift"
+            ]
+        )
+    ]
 )
